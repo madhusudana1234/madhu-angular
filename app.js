b8ffa7cc251ff22ea89e227c3ac80977e82003c4
madhusudana1234 <106326400+madhusudana1234@users.noreply.github.com>
2022-06-03 06:53:57 +0530

    Add files via upload
    adding html code

---
+++ app.js
@@ -0,0 +1,235 @@
+
+// operators
+ 
+var a = 8 + 6;
+ 
+console.log(a);
+ 
+ 
+function add(a, b) {
+ 
+   
+ console.log(a,b);
+    return {
+        color: "yellow",
+        brand: "swift",
+        number: 9654,
+        engineCondition: "Good",
+        address: {
+            street: "leelamahal",
+            city: "Tirupathi"
+        }
+ 
+    };
+ 
+    var result = a + b;
+    console.log(result);
+    console.log(a, b);
+ 
+}
+ 
+// calling time passing values is nothing but arguments
+var x = add({
+    color: "yellow",
+        brand: "swift",
+        number: 9654,
+        engineCondition: "Good",
+        address: {
+            street: "leelamahal",
+            city: "Tirupathi"
+}
+},{
+    firstName:"Madhu",
+    lastName:"sudana"
+}
+);
+ 
+console.log(x);
+ 
+ 
+var a= 5+5*8;
+ 
+    a= 5+40;
+ 
+var q= (8+5)*7;
+ 
+true<3
+ 
+var a= 1<2<3;
+ 
+console.log(a);
+ 
+ 
+var a= 4 ;
+ 
+var b= 5;
+ 
+var c=6;
+ 
+ 
+ var t= a=b=c;
+ 
+ console.log(a);
+ 
+ console.log(b)
+ 
+ console.log(c);
+ 
+ console.log(t);
+
+// operators
+ 
+var a = 9 + 6;
+ 
+console.log(a);
+ 
+ 
+function add(a, b) {
+ 
+   
+ console.log(a,b);
+    return {
+        color: "black",
+        brand: "amaron",
+        number: 8569,
+        engineCondition: "Good",
+        address: {
+            street: "karakambadi",
+            city: "Tirupathi"
+        }
+ 
+    };
+ 
+    var result = a + b;
+    console.log(result);
+    console.log(a, b);
+ 
+}
+ 
+// calling time passing values is nothing but arguments
+var x = add({
+    ccolor: "black",
+    brand: "amaron",
+    number: 8569,
+    engineCondition: "Good",
+    address: {
+        street: "karakambadi",
+        city: "Tirupathi"
+}
+},{
+    firstName:"sidhu",
+    lastName:"yerra"
+}
+);
+ 
+console.log(x);
+ 
+ 
+var a= 3+8*6;
+ 
+    a= 3+48;
+ 
+var q= (5+5)*6;
+ 
+true<3
+ 
+var a= 1<2<3;
+ 
+console.log(a);
+ 
+ 
+var a= 6 ;
+ 
+var b= 7;
+ 
+var c=8;
+ 
+ 
+ var t= a=b=c;
+ 
+ console.log(a);
+ 
+ console.log(b)
+ 
+ console.log(c);
+ 
+ console.log(t);
+
+// operators
+ 
+var a = 9 + 5;
+ 
+console.log(a);
+ 
+ 
+function add(a, b) {
+ 
+   
+ console.log(a,b);
+    return {
+        color: "green",
+        brand: "label",
+        number: 9689,
+        labelCondition: "Good",
+        address: {
+            street: "leelamahal",
+            city: "Tirupathi"
+        }
+ 
+    };
+ 
+    var result = a + b;
+    console.log(result);
+    console.log(a, b);
+ 
+}
+ 
+// calling time passing values is nothing but arguments
+var x = add({
+    color: "green",
+        brand: "label",
+        number: 9689,
+        labelCondition: "Good",
+        address: {
+            street: "leelamahal",
+            city: "Tirupathi"
+}
+},{
+    firstName:"nanda",
+    lastName:"bbau"
+}
+);
+ 
+console.log(x);
+ 
+ 
+var a= 6+5*6;
+ 
+    a= 6+30;
+ 
+var q= (4+5)*6;
+ 
+true<3
+ 
+var a= 1<2<3;
+ 
+console.log(a);
+ 
+ 
+var a= 8;
+ 
+var b= 3;
+ 
+var c=6;
+ 
+ 
+ var t= a=b=c;
+ 
+ console.log(a);
+ 
+ console.log(b)
+ 
+ console.log(c);
+ 
+ console.log(t);
+
